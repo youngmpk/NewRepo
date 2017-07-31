00aedf27494e759bea18183fed42f9e32784744a
youngmpk <youngmpk@gmail.com>
2017-07-31 13:26:47 +0900

    First commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Countries =  new List<string>()
+            {
+                "India",
+                "US",
+                "UK"
+
+            };
+            return View();
+        }
+
+        public ViewResult Index2()
+        {
+            ViewData["Countries"] = new List<string>()
+            {
+                "India",
+                "US",
+                "UK"
+
+            };
+            return View();
+        }
+
+
+
+
+
+
+
+    }
+}
